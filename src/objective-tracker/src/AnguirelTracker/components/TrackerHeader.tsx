9e634f47453d3c6bce9a649c3302fa9c3833ba03
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-03-17 14:32:16 -0400

    update version

--- src/objective-tracker/src/AnguirelTracker/components/TrackerHeader.tsx
+++ src/objective-tracker/src/AnguirelTracker/components/TrackerHeader.tsx
@@ -65,7 +65,7 @@ export function TrackerHeader(props: Props): JSX.Element {
         setCharacterTag,
     } = settings;
 
-    const modeDisplay = `v1.1`;
+    const modeDisplay = `v1.2`;
 
     const [showOptions, setShowOptions] = useState(false);
     const [showShare, setShowShare] = useState(false);
